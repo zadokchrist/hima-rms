019ba8f8f39efc3c78bf09c3dec4e328d95a682f
DANIEL NGOBI <ngobizadokchrist@gmail.com>
2021-06-30 11:39:46 +0300

    initial commit

---
+++ HIMARMS/Controllers/MiningLicenseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HIMARMS.Controllers
+{
+    public class MiningLicenseController : Controller
+    {
+        // GET: MiningLicense
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
